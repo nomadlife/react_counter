f7a8e076e00a6f12840fbc762a7356ed67789cbf
nomadlife_15w <joonwoo3816@gmail.com>
2019-12-31 10:08:09 +0900

    map function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        count : 0
+        count : 0,
         // imageUrl: 'https://picsum.photos/200'
+        tags:['tag1','tag2','tag3']
     };
 
     styles = {
@@ -18,6 +19,9 @@ class Counter extends Component {
                 {/* <img src={this.state.imageUrl} alt=""></img> */}
                 <span style={{fontSize:30}} className={this.getBadgeClasses()}> {this.formatCount()} </span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
+                    { this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul>
             </React.Fragment>
         )
     }
