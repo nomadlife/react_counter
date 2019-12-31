33b9aabb430ed1a1baa4aa0c7b04fad6fe840663
nomadlife_15w <joonwoo3816@gmail.com>
2019-12-31 19:17:02 +0900

    test for single source of state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -14,13 +14,21 @@ export class Counters extends Component {
   handleDelete = counterId => {
     // console.log("Delete Event Handler Called", counterId);
     const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({
-        counters
-        });
+    this.setState({ counters });
   };
+
+  handleReset = () => {
+    const counters = this.state.counters.filter(c => {
+        c.value = 0;
+        return c;
+    });
+    this.setState({ counters });
+  }
+
   render() {
     return (
       <div>
+          <button className="btn btn-primary btn-sm m-2" onclick={this.handleReset}></button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
