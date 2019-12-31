073d81e99af7ba4f8f4048dadd9046e599142485
nomadlife_15w <joonwoo3816@gmail.com>
2019-12-31 10:57:25 +0900

    conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,8 +3,7 @@ import React, { Component } from 'react'
 class Counter extends Component {
     state = {
         count : 0,
-        // imageUrl: 'https://picsum.photos/200'
-        tags:['tag1','tag2','tag3']
+        tags:[]
     };
 
     styles = {
@@ -12,16 +11,17 @@ class Counter extends Component {
         fontWeight : 'bold'
     };
 
+    renderTags(){
+        if (this.state.tags.length === 0) return <p>There are no tags!!</p>;
+        return <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+
+    }
+
     render() {
-        // let classes = this.getBadgeClasses(); 
         return (
             <React.Fragment>
-                {/* <img src={this.state.imageUrl} alt=""></img> */}
-                <span style={{fontSize:30}} className={this.getBadgeClasses()}> {this.formatCount()} </span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    { this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
+                { this.state.tags.length === 0 && 'Please create a new tag!'}
+                {this.renderTags()}
             </React.Fragment>
         )
     }
